3686d034368bedbed012889879251a453976f147
max <maximogarciamtnez@gmail.com>
2018-05-14 01:28:02 +0200

    added toggleMenu

---
+++ src/app/header/sm/menu/menu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+  static isOpen: boolean = false;
+
+  constructor() {}
+  getStatus = (): boolean => {
+    return MenuComponent.isOpen;
+  };
+  toogleMenu = () => {
+    MenuComponent.isOpen = !MenuComponent.isOpen;
+  };
+  ngOnInit() {}
+}
