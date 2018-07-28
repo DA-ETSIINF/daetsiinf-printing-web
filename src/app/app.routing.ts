9252253f43f5ad0eb6372ee713db48f5e3b3ce7f
max <maximogarciamtnez@gmail.com>
2018-07-28 13:57:47 +0200

    added register

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,12 +4,14 @@ import { ModuleWithProviders } from '@angular/core';
 import { MyDocumentsComponent } from './documents/my-documents/my-documents.component';
 import { SharedDocumentsComponent } from './documents/shared-documents/shared-documents.component';
 import { LoginComponent } from './login/login/login.component';
+import { RegisterComponent } from './login/register/register.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: 'my-documents', component: MyDocumentsComponent },
   { path: 'shared-with-me', component: SharedDocumentsComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '', component: AppComponent }
 ];
 
