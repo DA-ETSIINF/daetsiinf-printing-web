6a56baea09cdeb39e066292781d15e601aacf69f
Max <maximogarciamtnez@gmail.com>
2018-12-05 08:33:09 +0100

    modified price in queue for /quick page

--- src/app/files/queue/queue.component.ts
+++ src/app/files/queue/queue.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FilesService } from '../files.service';
 import { AppComponent } from '../../app.component';
 import { FileToPrint } from '../../models';
+import { UserService } from '../../user.service';
+import { UserInfo } from '../../models';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-queue',
@@ -13,14 +16,25 @@ export class QueueComponent implements OnInit {
   step = 'first';
   lastChanged = new Date().getTime();
   filesInQueue: FileToPrint[] = [];
+  userInfo: UserInfo;
 
+  // This var represents if we need to show the funds.
+  showMyMoney = true;
   constructor(
     private appComponent: AppComponent,
-    private filesService: FilesService
+    private filesService: FilesService,
+    private userService: UserService,
+    private router: Router
   ) {
     this.filesService.itemsInQueue$.subscribe(e => {
       this.filesInQueue = e;
     });
+    const url = this.router.routerState.snapshot.url;
+    if (url !== '/quick') {
+      this.userService.userInfo$.subscribe(i => this.userInfo = i);
+    } else {
+      this.showMyMoney = false;
+    }
   }
 
   ngOnInit() {}
