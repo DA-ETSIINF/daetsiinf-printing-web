0ce7002b68199659fdae048f4f03be3c822b6d3d
Max <maximogarciamtnez@gmail.com>
2018-12-05 11:06:27 +0100

    modified menu in header-lh

--- src/app/general_components/header/lg/header-lg.component.ts
+++ src/app/general_components/header/lg/header-lg.component.ts
@@ -8,26 +8,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./header-lg.component.css']
 })
 export class HeaderLgComponent {
-  showUserDropdown = false;
-  hideUserDropdown = false;
+  showUserDropdown = true;
   version: string = environment.version;
 
   @Input()
   userInfo: UserInfo;
 
   constructor(private router: Router) {}
-
-  setShowUserDropdown() {
-    this.showUserDropdown = true;
-    this.hideUserDropdown = true;
-  }
-
-  setHideUserDropdown() {
-    this.hideUserDropdown = false;
-    setTimeout(function() {
-      this.showUserDropdown = false;
-    }, 100);
-  }
 
   goTo(route: string) {
     this.router.navigate([route]);
